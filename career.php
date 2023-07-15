c5f1db4827cee149359c9fc42dd4f05272898daa
Vishalincred <vishal.pal@incred.com>
2023-07-15 11:26:39 +0530

    deploy on 15JUL

---
+++ career.php
@@ -0,0 +1,445 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+	<!-- Basic Page Needs
+	================================================== -->
+	<meta charset="utf-8">
+    <title>Career</title>
+    <meta name="description" content="">	
+	<meta name="author" content="">
+
+	<!-- Mobile Specific Metas
+	================================================== -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Favicons
+	================================================== -->
+	<link rel="icon" href="img/favicon/favicon-32x32" type="image/x-icon" />
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/favicon-144x144">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/favicon-72x72">
+	<link rel="apple-touch-icon-precomposed" href="img/favicon/favicon-54x54">
+	
+	<!-- CSS
+	================================================== -->
+	
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<!-- Template styles-->
+	<link rel="stylesheet" href="css/style.css">
+	<!-- Responsive styles-->
+	<link rel="stylesheet" href="css/responsive.css">
+	<!-- FontAwesome -->
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<!-- Animation -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Prettyphoto -->
+	<link rel="stylesheet" href="css/prettyPhoto.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/owl.theme.css">
+	<!-- Flexslider -->
+	<link rel="stylesheet" href="css/flexslider.css">
+	<!-- Flexslider -->
+	<link rel="stylesheet" href="css/cd-hero.css">
+	<!-- Style Swicther -->
+	<link id="style-switch" href="css/presets/preset3.css" media="screen" rel="stylesheet" type="text/css">
+
+	
+
+</head>
+	
+<body>
+	
+	 <?php include("includes/header.php");  ?>
+	
+	<div class="body-inner">
+	<!-- Header start -->
+	
+
+	<div id="banner-area">
+		<img src="images/banner/banner2.jpg" alt ="" />
+		<div class="parallax-overlay"></div>
+			<!-- Subpage title start -->
+			<div class="banner-title-content">
+	        	<div class="text-center">
+		        	<h2>Career</h2>
+		        	<ul class="breadcrumb">
+			            <li>Home</li>
+			           
+			            <li><a href="#"> Career</a></li>
+		          	</ul>
+	          	</div>
+          	</div><!-- Subpage title end -->
+	</div><!-- Banner area end -->
+
+
+
+
+
+<div class="cut_main22">
+           <div class="container">
+		   
+		    <div class="row">
+			 <div class="col-md-12">
+			 
+			 <div class="hiring_mn">
+                
+				<h2>WE ARE HIRING !</h2>
+				<p>We offer IT services and ready-to-use Web hosted software systems for a number of verticals which can be customized easily to meet your specific needs.</p>
+				
+				
+				
+            </div>
+			</div>
+           
+        </div>
+		 </div>
+		  </div>
+		
+
+ 
+  <section class="career_mn">
+				<div class="container">
+						
+					
+
+						
+						
+						 <div id="tab_mn1"  class="panel panel-default">
+			                <div class="panel-heading">
+				                <h4 class="panel-title">
+				                	<a data-toggle="collapse" class="collapsed" data-parent="#accordionA, #accordionB" href="#collapseC"> ASP Developer</a>
+				            	</h4>
+			                </div>
+			                <div id="collapseC" class="panel-collapse collapse" style="height: 0px;">
+			                  <div class="panel-body">
+			                   
+							   
+							<div class="row borders">
+				<div class="col-md-3 pad">
+					<strong>Requirement:</strong>
+				</div>
+				<div class="col-md-9 pad"> Software Developer</div>
+				
+				
+				<div class="col-md-3 pad">
+					<strong>Positions:</strong>
+				</div>
+				<div class="col-md-9 pad">ASP Developer </div>
+				<div class="col-md-3 pad">
+					<strong>Location:</strong>
+				</div>
+				<div class="col-md-9 pad">Gurgaon</div>
+				
+				
+				<div class="pad">
+				<td class="" colspan="3"><p><strong><br>Job Description: </strong></p>Develops and maintains applications and databases by evaluating client needs; analyzing requirements; developin software systems. 
+Enhances staff accomplishments and competence by planning delivery of solutions; answering technical and procedural questions for less experienced team members; teaching improved processes; mentoring team members. 
+Identifies requirements by establishing personal rapport with potential and actual clients and with other persons in a position to understand service requirements. 
+Arranges project requirements in programming sequence by analyzing requirements; preparing a workflow chart and diagram using knowledge of computer capabilities, subject matter, programming language, and logic. 
+Programs the computer by encoding project requirements in computer language; entering coded information into the computer. 
+Confirms program operation by conducting tests; modifying program sequence and/or codes. 
+Provides reference for use of prime and personal computers by writing and maintaining user documentation; maintaining a help desk. 
+Maintains computer systems and programming guidelines by writing and updating policies and procedures. 
+Maintains professional and technical knowledge by attending educational workshops; reviewing professional publications; establishing personal networks; participating in professional societies. 
+Protects operations by keeping information confidential. 
+Contributes to team effort by accomplishing related results as needed. 
+Highly analytical with excellent problem-solving capabilities 
+High attention to detail Excellent organization and communication skills 
+Ability to work independently on multiple assignments with minimal supervision<p>
+<br>
+<strong>Experience:</strong>
+
+</p><ul><li>1 to 5 years programming experience using Classic ASP 3.0 and skills around ASP. Net, </li>
+
+<li>1 to 3 years using JavaScript while experience on Ajax, Json, CSS will be a nice to have. </li>
+
+<li>C# etc. would be an added advantage.  </li>
+<li>1+ years writing medium to high complexity RDMS/SQL queries, or equivalent</li>
+
+
+<br>
+</ul><p><strong>Qualification:</strong> Professional Degree, Other Bachelor Degree</p></td>
+
+
+<strong>Skills:</strong>
+
+<div class="skil_mn">
+<ul>
+
+<li>Classic ASP (absolute mandatory) </li>
+<li>Classic ASP </li>
+<li> VB 6.0 </li>
+<li>HTML </li>
+<li>JAVASCRIPT </li>
+<li> SQL Server </li>
+<li> Industry </li>
+<li> IT Software - Application Programming / Maintenance</li>
+
+</ul>
+
+</div>
+				
+				</div>
+				
+				
+				
+				
+			</div>
+							   
+							   
+							   
+			                  </div>
+			                </div>
+		              	</div>
+						
+						
+						
+						 <div id="tab_mn1"  class="panel panel-default">
+			                <div class="panel-heading">
+				                <h4 class="panel-title">
+				                	<a data-toggle="collapse" class="collapsed" data-parent="#accordionB, #accordionD" href="#collapseD"> React Developer</a>
+				            	</h4>
+			                </div>
+			                <div id="collapseD" class="panel-collapse collapse" style="height: 0px;">
+			                  <div class="panel-body">
+			                   
+							   
+							<div class="row borders">
+				<div class="col-md-3 pad">
+					<strong>Requirement:</strong>
+				</div>
+				<div class="col-md-9 pad"> Software Developer</div>
+				
+				
+				<div class="col-md-3 pad">
+					<strong>Positions:</strong>
+				</div>
+				<div class="col-md-9 pad">React Developer</div>
+				<div class="col-md-3 pad">
+					<strong>Location:</strong>
+				</div>
+				<div class="col-md-9 pad">Gurgaon</div>
+				
+				
+				<div class="pad">
+				<td class="" colspan="3"><p><strong><br>Job Description: </p></strong>
+				
+				<p>We are looking for a great JavaScript developer who is proficient with React.js. Your primary focus will be on developing user interface components and implementing them following well-known React.js workflows (such as Flux or Redux). You will ensure that these components and the overall application are robust and easy to maintain. You will coordinate with the rest of the team working on different layers of the infrastructure. Therefore, a commitment to collaborative problem-solving, sophisticated design and quality products is important.
+Job Responsibilities Building efficient, reusable well-designed, and high-quality front-end systems using React.js .Translating designs and wireframes into high-quality code Designing highly responsive web-based user interfaces Building reusable components and front-end libraries for future Debugging & optimizing code to enable maximum performance across devices and browsers. Providing code documentation and other inputs to technical documents. Participating in code reviews.</p>
+<br>
+<strong>Experience:</strong>
+
+<p style="font-size:17px; font-weight:400; margin-top:5px;">3 + Yrs</p>
+
+
+
+
+
+
+
+<strong>Skills:</strong>
+
+<div class="skil_mn">
+<ul>
+
+<li>Bachelor’s/Master’s degree in computer science or engineering with a focus on language processing</li>
+<li>Strong proficiency in JavaScript, including DOM manipulation and the JavaScript object model </li>
+<li> Thorough understanding of React.js and its core principles</li>
+<li>Experience with data structure libraries (e.g., Immutable.js) </li>
+<li>Knowledge of isomorphic React is a plus </li>
+<li> Knowledge of modern authorization mechanisms, such as JSON Web Token </li>
+<li> Familiarity with modern front-end build pipelines and tools </li>
+<li> Ability to understand business requirements and translate them into technical requirements</li>
+
+<li>You have solid 2-3 years of experience in React.js as well as experience in React.js workflows (Flux/Redux)</li>
+<li>Familiarity with RESTful APIs</li>
+<li>You are experienced in using code versioning tools such as Git or equivalent</li>
+<li>Familiarity with various testing tools.</li>
+<li>You possess strong attention to detail and take pride in delivering high-quality work in a timely manner</li>
+
+</ul>
+
+</div>
+				
+				</div>
+				
+				
+				
+				
+			</div>
+							   
+							   
+							   
+			                  </div>
+			                </div>
+		              	</div>
+						
+						
+						   
+			                </div>
+							
+		              	</section>
+ 
+ 
+		 
+		 <section id="contact" class="atf-contact-area atf-section-padding pddng">
+				<div class="container">
+					
+					
+					<div class="row align-items-center">
+						<div class="col-lg-7 col-12 " data-aos="fade-up">
+							<div class="contact">
+								<h4>Send a Message</h4>
+								<form id="contact-form" class="atf-contact-form form" method="POST" action="mail.php">
+									
+									<div class="row">
+									<div class="form-group col-md-12">
+										<div class="form-group col-md-6">
+											<input type="text" name="name" class="form-control" id="name" placeholder="First Name" required="required">
+										</div>
+										
+										<div class="form-group col-md-6">
+											<input type="text" name="Last Name" class="form-control" id="last name" placeholder="Last Name" required="required">
+										</div>
+										
+										
+										</div>
+										</div>
+										
+										
+										<div class="row">
+									<div class="form-group col-md-12">
+										<div class="form-group col-md-6">
+											<input type="email" name="email" class="form-control" id="form_email" placeholder="E-Mail Address" required="required">
+										</div>
+										
+										<div class="form-group col-md-6">
+											<input class="form-control" data-val="true" data-val-regex="Entered phone format is not valid." data-val-regex-pattern="^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$" data-val-required="Telephone Number Required" id="Phone" name="Phone" placeholder="Enter your Number" type="text" value="" fdprocessedid="20izbl">
+										</div>
+										
+										
+										</div>
+										</div>
+										
+										
+										<div class="row">
+									<div class="form-group col-md-12">
+										<div class="form-group col-md-6">
+											<input class="form-control" id="Address" name="Address" placeholder="Address" type="text" value="" fdprocessedid="uzypao">
+										</div>
+										
+										<div class="form-group col-md-6">
+											<div class="input-group">
+                                <span class="input-group-addon adon"><i class="fa fa-file-text" aria-hidden="true"></i></span>
+                                <input type="file" id="File" class="form-control fil_mn" name="File" placeholder="Upload Resume">
+                                
+                            </div>
+										</div>
+										
+										
+										</div>
+										</div>
+										
+										
+										
+										
+										<div class="form-group col-md-12">
+											<textarea rows="6" name="message" class="form-control" id="message" placeholder="Description" required="required"></textarea>
+										</div>
+										<div class="col-md-12">
+											<div class="text-left">
+												<button type="submit" value="Submit Now" name="submit" id="submitButton" class="btn atf-themes-btn read2" title="Submit Your Message!"><span>Send Enquiry <span class="fa fa-send"></span></span></button>
+											</div>
+										</div>
+									
+								</form>
+								<p class="form-message"></p>
+							</div>
+						</div><!-- END COL -->
+						
+						<div class="col-lg-5 ps-lg-5">
+							<div class="atf-contact-info">
+								<div class="atf-section-title atf-section-title2">
+									<h4>Our Address</h4>
+								</div>
+							
+								<div class="atf-contact-details">
+									<i class="fa fa-home"></i>
+									<h4>Your Location</h4>
+									<p>Address: Orbitsys Technologies Pvt. Ltd.
+									507, Tower-A, Iris Tech Park Sector 48,
+									Sohna Road Gurgaon - Haryana
+									(122001) India</p>
+									
+								</div>
+							
+								<div class="atf-contact-details">
+									<i class="fa fa-phone"></i>
+									<h4>Phone Number</h4>
+									<p>+91 124 4283162</p>
+									<p></p>
+								</div>
+						
+								<div class="atf-contact-details">
+									<i class="fa fa-envelope"></i>
+									<h4>Email Address</h4>
+									<p>marketing@orbitsys.co.in</p>
+								</div>
+								
+								
+								
+							</div><!--- END CONTACT -->
+						</div><!--- END COL -->
+					</div><!--- END ROW -->
+				</div><!--- END CONTAINER -->
+			</section>
+		 
+	
+
+
+	<!-- Footer start -->
+	
+	<?php include("includes/footer.php");  ?>
+
+	
+	<!-- Javascript Files
+	================================================== -->
+
+	<!-- initialize jQuery Library -->
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<!-- Bootstrap jQuery -->
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<!-- Style Switcher -->
+	<script type="text/javascript" src="js/style-switcher.js"></script>
+	<!-- Owl Carousel -->
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+	<!-- PrettyPhoto -->
+	<script type="text/javascript" src="js/jquery.prettyPhoto.js"></script>
+	<!-- Bxslider -->
+	<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	<!-- CD Hero slider -->
+	<script type="text/javascript" src="js/cd-hero.js"></script>
+	<!-- Isotope -->
+	<script type="text/javascript" src="js/isotope.js"></script>
+	<script type="text/javascript" src="js/ini.isotope.js"></script>
+	<!-- Wow Animation -->
+	<script type="text/javascript" src="js/wow.min.js"></script>
+	<!-- SmoothScroll -->
+	<script type="text/javascript" src="js/smoothscroll.js"></script>
+	<!-- Eeasing -->
+	<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+	<!-- Counter -->
+	<script type="text/javascript" src="js/jquery.counterup.min.js"></script>
+	<!-- Waypoints -->
+	<script type="text/javascript" src="js/waypoints.min.js"></script>
+	<!-- Template custom -->
+	<script type="text/javascript" src="js/custom.js"></script>
+	</div><!-- Body inner end -->
+</body>
+
+
+</html>
